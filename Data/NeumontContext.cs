7e5e2577e6d65566d0ecfc87bd5bb73ce8ca6c82
lawlietl4 <nroberson7712@gmail.com>
2021-05-03 18:05:55 -0600

    Add project files.

---
+++ Data/NeumontContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testSQLPoco.Models;
+
+namespace testSQLPoco.Data
+{
+    class NeumontContext : DbContext
+    {
+        private readonly string host = "localhost";
+        private readonly string db = "NeumontClass";
+        private readonly string username = "neumontuser";
+        private readonly string password = "neumontpassword";
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql($"Host={host};Database={db};Username={username};Password={password};");
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using testSQLPoco.Data;
+
+namespace testSQLPoco
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly NeumontContext _context = new NeumontContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //checks if there are new things in the migration folder to be added to the DB
+            _context.Database.Migrate();
+
+            _context.Students.Load();
+            studentsViewSource.Source = _context.Students.Local.ToObservableCollection();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _context.Dispose();
+            base.OnClosing(e);
+        }
+    }
+}
